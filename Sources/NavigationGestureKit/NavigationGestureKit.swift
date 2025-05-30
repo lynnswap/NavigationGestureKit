0158d89870841abf6e0bc5f6dd6c75cf076d88ae
Lynn <65545348+lynnswap@users.noreply.github.com>
2025-05-30 14:43:05 +0900

    Add add/remove API for WebView back swipe gesture

--- Sources/NavigationGestureKit/NavigationGestureKit.swift
+++ Sources/NavigationGestureKit/NavigationGestureKit.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+/// Namespace for package level constants.
+public enum NavigationGestureKit {
+    /// Default name assigned to the WebView full-width back swipe gesture.
+    public static let defaultFullWidthBackGestureName = "NGKWebViewBackSwipe"
+}
